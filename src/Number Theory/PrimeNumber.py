f647389caf6617cace537c1d25279e26d3d60563
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-20 02:15:25 +0300

    one prime number

--- src/Number Theory/PrimeNumber.py
+++ src/Number Theory/PrimeNumber.py
@@ -1,4 +1,16 @@
-#Simple Number
+#Prime Number
+
+#One
+def isPrime(n):
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
+
+
+
 #Sieve of Eratosthenes
 
 n = int(input())
