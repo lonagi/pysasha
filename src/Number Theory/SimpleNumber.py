a2dce0410190dad42e4bcbb67dee1b08a6a2f343
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-20 01:59:47 +0300

    Sieve of Eratosthenes

---
+++ src/Number Theory/SimpleNumber.py
@@ -0,0 +1,16 @@
+#Simple Number
+#Sieve of Eratosthenes
+
+n = int(input())
+a = list(range(n+1))
+a[1]=0
+lst = []
+
+i=2
+while i<=n:
+    if a[i]!=0:
+        lst.append(a[i])
+        for j in range(i,n+1,i):
+            a[j]=0
+    i+=1
+print(lst)
