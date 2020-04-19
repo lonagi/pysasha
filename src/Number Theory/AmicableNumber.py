cd0e2a9e82b8d93d217f104acd91fceb2c54a46c
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-20 01:05:59 +0300

    Update AmicableNumber.py

--- src/Number Theory/AmicableNumber.py
+++ src/Number Theory/AmicableNumber.py
@@ -1,30 +1,41 @@
 #Amicable numbers
 
 #Brute force Method
-def AmicableNumber():
-    ##Start loop from...
-    start = 1
-
-    ##Target for loop is...
-    purpose = 30000
-
+def AmicableNumber(k,returni=False):  
     ##All divisors for all numbers
     allDels = dict()
+    
+    ##Second number is greater than first number
+    try:
+        from itertools import chain
+    except:
+        pass
+    concatenated = chain( range(k, int(k*2.5)+1 ),range(k, int(k/3)+1 ,-1) )
+    for i in concatenated:
 
-    ##First number
-    for k in range(start,purpose+1):            
+        ##We don't want repeat operations
+        ##We search all divisors
+        if(str(i) not in allDels):
+            allDels[str(i)] = set([1])
+            for j in range(k-1,1,-1):
+                if(i!=j and i%j==0):
+                    allDels[str(i)].add(j)
 
-        ##Second number is greater than first number
-        for i in range(k,int( (purpose+1)/3 )):
+        ##Sum1 = 1st_num and Sum2 = 2nd_num
+        if(i != k and sum(allDels[str(i)]) == k and sum(allDels[str(k)]) == i):
+            if(returni):
+                return (k,i)
+            else:
+                print(k,"->",i)    
 
-            ##We don't want repeat operations
-            ##We search all divisors
-            if(str(i) not in allDels):
-                allDels[str(i)] = set([1])
-                for j in range(2,int((purpose+1)/2)+1):
-                    if(i!=j and i%j==0):
-                        allDels[str(i)].add(j)
-
-            ##Sum1 = 1st_num and Sum2 = 2nd_num
-            if(i != k and sum(allDels[str(i)]) == k and sum(allDels[str(k)]) == i):
-                print(i,k)
+#AmicableNumber(220)
+                
+allPairs = set()
+_temp = 0
+for i in range(2,285):
+    _temp = AmicableNumber(i,True)
+    if(_temp != None):
+        allPairs.add(_temp)
+        
+allPairs = {tuple(sorted(item)) for item in allPairs}
+print(allPairs)
