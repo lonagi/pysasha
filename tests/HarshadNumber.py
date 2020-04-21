7860d9598c3f906d2d2b27a2b659e290ae7f2b22
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-22 00:02:45 +0300

    Create HarshadNumber.py

---
+++ tests/HarshadNumber.py
@@ -0,0 +1,10 @@
+for num in range(10,15001):
+    rem = sum = 0
+    n = num
+    while(num > 0):    
+        rem = num%10
+        sum = sum + rem   
+        num = num//10  
+
+    if(n%sum == 0):    
+        print(str(n),end=", ")
