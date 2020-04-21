e49d02d6d8fc38ec40ceab5d962b5eb2089cbc5f
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-21 13:29:33 +0300

    New algo for perfect search

--- src/Number Theory/PerfectNumber.py
+++ src/Number Theory/PerfectNumber.py
@@ -1,43 +1,25 @@
-#Find Perfect number
+#Find Perfect Numbers
 
-def EnumEvenTerms(num, res2 = [], progress=False, prog_step = 10000):
-    res = []
-    for i in range(num-1,0,-1):
+##TOOLS
+def Divisors(num):
+    s = set([1])
+    for i in range( int(num/2 + 1),1,-1):
         if(num%i==0):
-            res.append(i)
-            
-    if(progress):
-        try:
-            from IPython.display import clear_output
-        except:
-            pass
-        
-        if(sum(res)==num):
-            res2.append( (num,res) )
-        
-        if(num % prog_step == 0 or sum(res)==num):
-            
-            clear_output(wait=True)
-            print("\tNum "+str(num))
+            s.add(i)
+    return s
 
-            for j in res2:
-                print("Num "+str(j[0])+": ",end="")
-                for i in j[1]:
-                    print(i,end="")
-                    if(i != res[-1]):
-                        print("+",end="")
-                print("")
-    else:
-        if(sum(res)==num):
-            print("Num "+str(num)+": ",end="")
-            for i in res:
-                print(i,end="")
-                if(i != res[-1]):
-                    print("+",end="")
-            print("")
-        return res
-    return res2
-    
-mynumbers = []
-for i in range(1,90000000000000000000000000000000000000000000000000000000000000000000):
-    mynumbers = EnumEvenTerms(i,mynumbers,True)
+##THE PROGRAM
+def PerfectNumber(num):
+    d = Divisors(num)
+    return sum(d)==num
+             
+def doTest(toPrint=False,start=2,toEnd=1000):
+    s = set()
+    for i in range(start,toEnd):
+        if(PerfectNumber(i)):
+            s.add(i)
+            if(toPrint):
+                print(i,end=", ")
+    return s
+            
+doTest(True,2,10000)
