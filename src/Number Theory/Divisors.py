0178d5dedfa6fb0cceb7636d099cf71b4131e5a3
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-21 13:18:50 +0300

    Create Divisors.py

---
+++ src/Number Theory/Divisors.py
@@ -0,0 +1,24 @@
+#Functions of divisors
+
+def Divisors(num):
+    s = set([1])
+    for i in range( int(num/2 + 1),1,-1):
+        if(num%i==0):
+            s.add(i)
+    return s
+
+#the number of the divisors of number
+def NumberOfDivisors(num):
+    return len(Divisors(num))+1
+
+#the sum of all the divisors of σ(number):
+def SumOfDivisors(num):
+    return sum(Divisors(num))+num
+
+#the aliquot sum s(number) of proper divisors
+def aSumOfDivisors(num):
+    return sum(Divisors(num))
+    
+print(NumberOfDivisors(12)) #6
+print(SumOfDivisors(12))    #28
+print(aSumOfDivisors(12))   #16
