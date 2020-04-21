03534962cf026479f78bb057ba2a68c48506f8ff
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-22 00:02:24 +0300

    Create ErdosNicolasNumber.py

---
+++ src/Number Theory/ErdosNicolasNumber.py
@@ -0,0 +1,51 @@
+########## Erdős–Nicolas number
+
+#TOOLs
+def Divisors(num):
+    s = set([1])
+    for i in range( int(num/2)+1,1,-1):
+        if(i != num and num%i==0):
+            s.add(i)
+    return s
+def PerfectNumber(num):
+    d = Divisors(num)
+    return sum(d)==num
+
+#
+##THE PROGRAM
+def ErdosNicolas_Number(num):
+    if(not PerfectNumber(num)):
+        s = list(Divisors(num))
+        s.sort()
+        for i in s:
+            if(sum(s) < num):
+                break
+            elif(sum(s)==num):
+                return True
+            s.pop()
+        return False
+    else:
+        return False
+    
+def doTest(toPrint=False,toProgress=False,start=2,toEnd=1000):
+    s = set()
+    KK = 10000
+    for i in range(start,toEnd+1):
+        if(toProgress and ( i < KK or (i>=KK and i % (KK/100) == 0)) ):
+            try:
+                from IPython.display import clear_output
+            except:
+                pass
+            clear_output(wait=True)
+            print(i,end="\t")
+        if(ErdosNicolas_Number(i) ):
+            s.add(i)
+            if(toPrint and not toProgress):
+                print(i,end=", ")
+        if(toProgress and ( i < KK or (i>=KK and i % (KK/100) == 0)) ):
+            print(s)
+    if(not toPrint):
+        return s
+    
+#print(ErdosNicolas_Number(24))
+doTest(True,False,2,43000)
