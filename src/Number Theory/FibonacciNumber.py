Fix Binet Formula

src/Number Theory/FibonacciNumber.py
@@ -19,8 +19,11 @@ def fib(index):
     F = pow(np.array(((1,1),(1,0)),dtype=object),index,np.eye(2))
     return F[0][1]
 
-def Binetformula(index):
-    return round((((5**1/2 + 1)/2)**index)/(5**1/2))
+def Binetformula(index,roundi=True):
+    if(roundi):
+        return round(( ((1+2.237)/2)**index - ((1-2.237)/2)**index )/2.237)
+    else:
+        return ( ((1+2.237)/2)**index - ((1-2.237)/2)**index )/2.237
 
 def doTest(toPrint=False,toProgress=False,start=0,toEnd=1000,algo="s"):
     s = set()
@@ -48,5 +51,5 @@ def doTest(toPrint=False,toProgress=False,start=0,toEnd=1000,algo="s"):
 
 #fib(8)
 #doTest(True,False,1,5000) #823 ms
-#Binetformula(5)
+#Binetformula(6)
 #doTest(True,False,1,5000,"binet") #163 ms