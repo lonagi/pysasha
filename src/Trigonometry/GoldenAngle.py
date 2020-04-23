effbfcee94f8ff3a82b5a4d6e9493f1c75539d9c
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-23 16:26:08 +0300

    Create GoldenAngle.py

---
+++ src/Trigonometry/GoldenAngle.py
@@ -0,0 +1,9 @@
+#The golden angle plays a significant role in the theory of phyllotaxis;
+#for example, the golden angle is the angle separating the florets on a sunflower.
+
+GoldenAngle = 360*(1-(1.236067977499789716775759127427135908658201562710016/2))
+print(GoldenAngle) # 137. degrees
+GoldenAngle/=360
+print(GoldenAngle) # 0.38 the fraction
+GoldenAngle*=2*3.14159
+print(GoldenAngle) # 2.39 radians
