68ea51ce5c6b140cc57fb2d4753ca24db7e800c6
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-23 18:40:03 +0300

    Plastic Number

---
+++ src/Number Theory/PlasticNumber.py
@@ -0,0 +1,13 @@
+def PlasticNumber(n):
+    pp = (((9+(69**(1/2)))/18)**(1/3))+(((9-(69**(1/2)))/18)**(1/3))
+    return round(n*pp)
+
+def PlasticNumberRatio(toEnd=100):
+    pp = (((9+(69**(1/2)))/18)**(1/3))+(((9-(69**(1/2)))/18)**(1/3))
+    for i in range(toEnd):
+        a = i*pp
+        if(a%1 *100 <10):
+            print(int(a),"/",i,end=', ')
+
+#PlasticNumberRatio(100)
+#PlasticNumber(80)
