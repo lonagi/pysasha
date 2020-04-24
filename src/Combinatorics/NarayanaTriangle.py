247a9befe147d712213749b998ad6932dc1baf71
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-24 16:43:20 +0300

    Update NarayanaTriangle.py

--- src/Combinatorics/NarayanaTriangle.py
+++ src/Combinatorics/NarayanaTriangle.py
@@ -1,6 +1,6 @@
 #Find Narayana Numbers
 
-def NarayanaNumbers(n,k):
+def NarayanaNumber(n,k):
     from math import factorial as mmfac
     from sympy import Float as mmmfloat
     from sympy import Integer as mmmint
@@ -15,17 +15,24 @@ def NarayanaNumbers(n,k):
     
     return _autoFloat((((mmfac(n))/((mmfac(k-1)) * (mmfac(n-k+1))))/n)*(mmfac(n)/((mmfac(k)*(mmfac(n-k))))))
 
-def printSequence(countt=10):
+def NarayanaSequence(countt=10,returni=False):
+    if(returni):
+        s = set()
     for k in range(1,countt):
         for n in range(k,countt):
-            print(NarayanaNumbers(n,k),end=", ")
+            if(not returni):
+                print(NarayanaNumber(n,k),end=", ")
+            else:
+                s.add(NarayanaNumber(n,k))
+    if(returni):
+        return s
         
 def NarayanaTriangle(countt=10):
     colk = [k for k in range(1,countt+1)]
     s=[['' for j in colk] for i in colk]
     for k in range(1,countt+1):
         for n in range(k,countt+1):
-            s[n-1][k-1] = NarayanaNumbers(n,k)
+            s[n-1][k-1] = NarayanaNumber(n,k)
             pass
     try:
         import pandas as mmpd
@@ -35,6 +42,6 @@ def NarayanaTriangle(countt=10):
     df =df.rename_axis(columns="n/k")
     return df
 
-#NarayanaNumbers(7,2)            
-#printSequence(100)
-NarayanaTriangle(300)
+#NarayanaNumber(7,2)
+#NarayanaSequence(100)
+#NarayanaTriangle(300)
