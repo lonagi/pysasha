de2fb6f8329f0065abcc290c19ef45127366bac3
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-27 17:28:58 +0300

    Update EvilNumber.py

--- src/Number Theory/EvilNumber.py
+++ src/Number Theory/EvilNumber.py
@@ -1,16 +1,25 @@
-#Find Evil numbers
-import time
+#Find Evil Numbers
 
-evils = []
-i = 3
-while True:
-    if(sum(map(lambda x : 1 if '1' in x else 0, str(bin(i))[2:] ))  % 2 == 0):
-        evils.append(i)
-    try:
-        from IPython.display import clear_output
-    except:
-        pass
-    clear_output(wait=True)
-    print(evils)
-    i+=1
-    time.sleep(0.1)
+def EvilNumber(num):
+    return sum(map(lambda x:1 if '1' in x else 0, str(bin(num))[2:]))%2==0
+    
+def doTest(toPrint=False,toProgress=False,start=0,toEnd=1000):
+    s=set()
+    KK=10000
+    from IPython.display import clear_output
+    for i in range(start,toEnd+1):
+        if(toProgress and (i<KK or (i>=KK and i%(KK/100)==0))):
+            clear_output(wait=True)
+            print(i,end="\t")
+        evil=EvilNumber(i)
+        if(evil):
+            s.add(i)
+            if(toPrint and not toProgress):
+                print(evil,end=", ")
+        if(toProgress and (i<KK or (i>=KK and i%(KK/100)==0))):
+            print(s)
+    if(not toPrint):
+        return s
+    
+#EvilNumber(3)
+#doTest()
