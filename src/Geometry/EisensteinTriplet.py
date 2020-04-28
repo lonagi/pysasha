90b47bb81508cb771bcd8f3fa445a9285adec840
Sasha <40319171+lonagi@users.noreply.github.com>
2020-04-29 01:03:36 +0300

    Update EisensteinTriplet.py

--- src/Geometry/EisensteinTriplet.py
+++ src/Geometry/EisensteinTriplet.py
@@ -1,5 +1,5 @@
 from math import sqrt as mmmsqrt
-def EisensteinTriplet(n,toInv=False):
+def EisensteinTriplet60(n,toInv=False):
     s=set()
     fr=0
     if(toInv):
@@ -10,5 +10,16 @@ def EisensteinTriplet(n,toInv=False):
             if c%1==0:
                 s.add((a, b, int(c)))
     return s
+def EisensteinTriplet120(n,toInv=False):
+    s=set()
+    fr=0
+    if(toInv):
+        fr=-n
+    for b in range(fr,n):
+        for a in range(fr+1, b):
+            c=mmmsqrt( a * a + b * b + a*b)
+            if c%1==0:
+                s.add((a, b, int(c)))
+    return s
     
-#EisensteinTriplet(5000) #4.19 s
+#EisensteinTriplet60(5000) #4.19 s
