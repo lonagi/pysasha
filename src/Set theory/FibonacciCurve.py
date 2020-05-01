864269bad280ae5e36e4c52a2ec9f5eb18f26fae
Sasha <40319171+lonagi@users.noreply.github.com>
2020-05-02 01:26:54 +0300

    Update FibonacciCurve.py

--- src/Set theory/FibonacciCurve.py
+++ src/Set theory/FibonacciCurve.py
@@ -1,3 +1,6 @@
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
 def IterationFibonacciWord(index):
     Sn_1 = "0"
     Sn = "01"
@@ -9,43 +12,46 @@ def IterationFibonacciWord(index):
     return Sn 
 ##########################################
 
-def initCanvas():
-	t = turtle.Turtle(fixed=False, width=N, height=N)
-	return t
+def initCanvas(N=600):
+    return turtle.Turtle(fixed=False, width=N, height=N)
 
-def setInitPos(rY=2.2,rX=2.2):
-	t.hideturtle()
-	t.penup()
-	t.back(N/rY)
-	t.right(90)
-	t.forward(N/rX)
-	t.left(90)
-	t.pendown()
+def setInitPos(t,rY=2.2,rX=2.2):
+    t.hideturtle()
+    t.penup()
+    t.back(N/rY)
+    t.right(90)
+    t.forward(N/rX)
+    t.left(90)
+    t.pendown()
+    return t
 
 def draw(indexMax=10,step=2,directionLeft=True,timewait=0.01):
-	j=0
-	k=0
-	while j<indexMax:
-	    if(k==0):
-	        myword='0'
-	    else:
-	        myword=IterationFibonacciWord(j)
-	    for i in myword:
-	        t.forward(step)
-	        if(i=='0'):
-	            if((directionLeft and k%2!=0) or (not directionLeft and k%2==0)):
-	                t.left(90)
-	            else:
-	                t.right(90)
-	        k+=1
-	    j+=1
-	    time.sleep(timewait)
-
-def main():
-	N=int(input("type size of window"))
-	initCanvas()
-	setInitPos()
-	draw()
+    j=0
+    k=0
+    while j<indexMax:
+        if(k==0):
+            myword='0'
+        else:
+            myword=IterationFibonacciWord(j)
+        for i in myword:
+            t.forward(step)
+            if(i=='0'):
+                if((directionLeft and k%2!=0) or (not directionLeft and k%2==0)):
+                    t.left(90)
+                else:
+                    t.right(90)
+            k+=1
+        j+=1
+        time.sleep(timewait)
 
 if(__name__=="__main__"):
-	main()
+    N=int(input("type size of window "))
+    while True:
+        indexMax=int(input("indexMax "))
+        if(indexMax < 50):
+            break
+    step=int(input("step "))
+    t=initCanvas(N)
+    t=setInitPos(t)
+    t
+    draw(indexMax=indexMax,step=step)
